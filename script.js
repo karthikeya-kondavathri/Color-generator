2314fb729444b9f8d4d3b0942d53d0508709716a
Karthikeya K <karthikkondavatri@gmail.com>
2024-11-22 17:17:42 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+        let backgroundColor = document.getElementById("colorPickerContainer");
+        let ColorHexCode = document.getElementById("selectedColorHexCode")
+
+        function SwitchColor1() {
+            backgroundColor.style.backgroundColor = "#e0e0e0";
+            ColorHexCode.textContent = "#e0e0e0";
+        }
+
+        function SwitchColor2() {
+            backgroundColor.style.backgroundColor = "#6fcf97";
+            ColorHexCode.textContent = "#6fcf97";
+        }
+
+        function SwitchColor3() {
+            backgroundColor.style.backgroundColor = "#56ccf2";
+            ColorHexCode.textContent = "#56ccf2";
+        }
+
+        function SwitchColor4() {
+            backgroundColor.style.backgroundColor = "#bb6bd9";
+            ColorHexCode.textContent = "#bb6bd9";
+        }
+
+      for (let i; i < a; i++){
+
+      }
